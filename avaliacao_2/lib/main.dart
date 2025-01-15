340b8acfd49c31a88777a98690a8cac463f73fe1
João Pedro Barbosa <info.joaopedrobarbosa@gmail.com>
2025-01-15 20:10:23 -0300

    feat: tela login

--- avaliacao_2/lib/main.dart
+++ avaliacao_2/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:avaliacao_2/src/features/cadastro/presentation/pages/tela_cadastro.dart';
+import 'package:avaliacao_2/src/features/login/presentation/pages/login.dart';
 import 'package:flutter/material.dart';
-import 'package:avaliacao_2/src/features/diario/presentation/pages/diario.dart';
+// import 'package:avaliacao_2/src/features/diario/presentation/pages/diario.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Cadastro',
-      home: TelaCadastro(),
+      title: 'Login',
+      home: TelaLogin(),
     );
   }
 }
