79cbf10552756d01728d922d2369afcd8ea38dd7
João Pedro Barbosa <info.joaopedrobarbosa@gmail.com>
2025-02-14 11:16:06 -0300

    feat: image picker

--- avaliacao_2/lib/src/features/editar_perfil/presentation/pages/editar_perfil_screen.dart
+++ avaliacao_2/lib/src/features/editar_perfil/presentation/pages/editar_perfil_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:typed_data';
+
 import 'package:avaliacao_2/src/features/cores/core/cores.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:avaliacao_2/src/features/editar_perfil/presentation/pages/utils.dart';
 
 class EditarPerfil extends StatefulWidget {
   const EditarPerfil({super.key});
@@ -12,9 +16,13 @@ class EditarPerfil extends StatefulWidget {
 
 class _EditarPerfilState extends State<EditarPerfil> {
   Color corBanner = Cores.branco;
+  Uint8List?  _image;
 
-  selecionarImagem() {
-    
+  selecionarImagem() async {
+    Uint8List img = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = img;
+    });
   }
 
   buildColorPicker() => ColorPicker(
@@ -72,19 +80,29 @@ class _EditarPerfilState extends State<EditarPerfil> {
           Stack(children: [
             SizedBox(
               height: 100,
-              width: 100,
-              child: CircleAvatar(
-                backgroundColor: Cores.roxo5,
-                child: IconButton(
-                    onPressed: selecionarImagem,
-                    icon: Icon(Icons.person_2_sharp,
-                        size: 50, color: Cores.branco50)),
-              ),
+              width: 100,              
+              child: 
+                _image != null ?
+                  CircleAvatar(
+                    radius: 64,
+                    backgroundImage: MemoryImage(_image!),
+                  )
+
+                :
+                  CircleAvatar(
+                    backgroundColor: Cores.roxo5,
+                    child: IconButton(
+                        onPressed: selecionarImagem,
+                        icon: Icon(Icons.person_2_sharp,
+                            size: 50, color: Cores.branco50)),
+                  ),
             ),
           ]),
           SizedBox(height: 15),
-          Text('Editar foto de perfil',
-              style: TextStyle(color: Cores.branco50)),
+          Text(
+            'Editar foto de perfil',
+            style: TextStyle(color: Cores.branco50)
+          ),
           SizedBox(height: 30),
           TextFormField(
             keyboardType: TextInputType.name,
